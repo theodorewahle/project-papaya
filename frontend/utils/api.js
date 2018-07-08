50b95aa627bc9a7888efc0a6bdae90d447671a1d
Shawn Vo <shawn.vo@gmail.com>
2018-07-08 11:22:07 -0400

    deployed backend on aws

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   //baseURL: 'http://206.189.196.178:5000/',
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://flask-env.ibah63x2sj.us-east-2.elasticbeanstalk.com/',
   timeout: 30000,
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
 });
